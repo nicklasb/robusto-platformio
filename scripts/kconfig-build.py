a0617a067328387453980d7e53045be2b1db2960
Nicklas Börjesson <nicklasb@gmail.com>
2023-04-11 23:51:06 +0200

    Reconstitute

--- scripts/kconfig-build.py
+++ scripts/kconfig-build.py
@@ -32,6 +32,21 @@ env.Append(CPPPATH=[build_dir])
 env.BuildSources(build_dir, build_dir)
 
 
+# If its not ESP-IDF, we add the settings header anyway so that the framework and applications don't have to care.
+if framework != "espidf":
+    # Check if there is anything for us to do here. TODO: Create the file if it is missing? Default configs?
+    kconfig_filename = os.path.join(project_dir, "Kconfig." + pio_env)
+    if os.path.isfile(kconfig_filename):
+        # Generate the config
+        print("KCONFIG_CONFIG=.config.{0} genconfig Kconfig.{0} --header-path {1}/robconfig_.h".format(pio_env, build_dir))
+        os.system("KCONFIG_CONFIG=.config.{0} genconfig Kconfig.{0} --header-path {1}/robconfig_.h".format(pio_env, build_dir))
+    else: 
+        print("Won't do any config, no", kconfig_filename, " file.")  
+else: 
+    print("Skipping ESP-IDF framework, it has its own Kconfig handling.")  
+
+
+
 def add_menu(source, target, env):
     curr_env = env.subst('$PIOENV')
     curr_dir = os.path.join(env.subst('$PROJECT_LIBDEPS_DIR'), curr_env, "Robusto-PlatformIO", "scripts")
