0e492624b909db264b41ca9a26204303afd4d76b
Nicklas Börjesson <nicklasb@gmail.com>
2023-04-12 20:59:41 +0200

    Add runner

---
+++ scripts/post-install-runner.py
@@ -0,0 +1,6 @@
+import os
+import subprocess
+cmd = os.environ["PIO_PYTHON_EXE"] + " " + os.path.join(os.environ["PWD"], "post-install.py")
+returned_value = subprocess.call(cmd)
+if returned_value != 0:
+    exit(returned_value)
--- scripts/post-install.py
+++ scripts/post-install.py
@@ -1,8 +1,5 @@
 import os
-result = ""
-for i, l in sorted(os.environ.items()):     
-    result += i + ' : ' + l + '\n'
+Import('env')
 
-file_text = open("output.txt", "w")
-file_text.write(result)
-file_text.close()
+print("IT WORKED")
+print(env.items())
