d21a0ee212f482cf05fd2050259cd5d12ae80edc
Nicklas Börjesson <nicklasb@gmail.com>
2023-04-12 21:21:49 +0200

    Fix param

--- scripts/post-install-runner.py
+++ scripts/post-install-runner.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
-cmd = os.environ["PIO_PYTHON_EXE"] + " " + os.path.join(os.environ["PWD"], "post-install.py")
-print("Run the PIO Python:" + cmd)
-returned_value = subprocess.call(cmd)
+cmd = os.environ["PIO_PYTHON_EXE"]
+param =os.path.join(os.environ["PWD"], "scripts", "post-install.py")
+print("Run the PIO Python:" + cmd + " " + param)
+returned_value = subprocess.call([cmd, param])
 if returned_value != 0:
     exit(returned_value)
