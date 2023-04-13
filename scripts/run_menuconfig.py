697c4729e1fc80c967c3f09f23461d5a7fbf243e
Nicklas Börjesson <nicklasb@gmail.com>
2023-04-13 19:20:38 +0200

    Liberate

--- scripts/run_menuconfig.py
+++ scripts/run_menuconfig.py
@@ -1,10 +1,18 @@
 import os
 
 import sys
-import shutil
-print(os.getcwd())
+import subprocess
+cwd =os.getcwd()
 # First, get our variables from the environment
 pio_env = sys.argv[1]
-#shutil.copy()
-os.system("cp -f ./scripts/Kconfig.template ./Kconfig.{0}".format(pio_env))
-os.system("KCONFIG_CONFIG=./.config.{0} menuconfig Kconfig.{0}".format(pio_env))
+template_filename = os.path.join(cwd, "scripts", "Kconfig.{0}".format(pio_env)))
+file = open(template_file, "w")
+file.write(os.path.join("components", "**", "Kconfig"))
+file.write(os.path.join("components", "**", "Kconfig.projbuild"))
+file.write(os.path.join(".pio", "libdeps", pio_env, "**", "Kconfig"))
+file.write(os.path.join(".pio", "libdeps", pio_env, "**", "Kconfig.projbuild"))
+file.close()
+
+# Todo
+config_filename = os.path.join(cwd, "scripts", "Kconfig.{0}".format(pio_env)))
+subprocess.call("menuconfig", env={"KCONFIG_CONFIG": config_filename}, args=["Kconfig.{0}".format(pio_env)])
