ee97a55810bcdc45da7a8662d0d9050be0206adf
Nicklas Börjesson <nicklasb@users.noreply.github.com>
2024-06-06 14:53:27 +0200

    Review menu

--- scripts/kconfig-build.py
+++ scripts/kconfig-build.py
@@ -13,7 +13,7 @@ build_dir = os.path.join(env.subst('$BUILD_DIR'), "config")
 framework = projenv.subst('$PIOFRAMEWORK')
 Kconfig_filename = os.path.join(project_dir, "Kconfig.{0}".format(pio_env))
 config_filename = os.path.join(project_dir, ".config.{0}".format(pio_env))
-
+targets = env.get("__PIO_TARGETS") 
 #print("--------------------------------------------------")
 #print(env)
 #print("--------------------------------------------------")
@@ -28,26 +28,20 @@ if not os.path.exists(build_dir):
   print("Create the build folder: {0}".format(build_dir))
   os.makedirs(build_dir)
 
-# Co we need to add a menuconfig target?
-targets = global_env.get("__PIO_TARGETS") or {}
 
-if not targets.values() and framework.lower() != "espidf":
-    if os.path.isfile(config_filename):
-        os.environ["KCONFIG_CONFIG"] = config_filename
-        build_dir = os.path.join(project_dir, ".pio", "build", pio_env)
-        gen_command = "genconfig {0} --header-path {1}".format(Kconfig_filename, os.path.join(build_dir, "config", "robconfig_.h"))
-        print("gen_command: ", gen_command)
-        os.system(gen_command)
-    exit()
-
-#def add_menu(source, target, env):
-print("IN ADD MENU ---------------------------------------")
-curr_env = env.subst('$PIOENV')
-curr_dir = os.path.join(env.subst('$PROJECT_LIBDEPS_DIR'), curr_env, "Robusto-PlatformIO", "scripts")
+if os.path.isfile(config_filename):
+    os.environ["KCONFIG_CONFIG"] = config_filename
+    build_dir = os.path.join(project_dir, ".pio", "build", pio_env)
+    gen_command = "genconfig {0} --header-path {1}".format(Kconfig_filename, os.path.join(build_dir, "config", "robconfig_.h"))
+    print("gen_command: ", gen_command)
+    os.system(gen_command)
 
 
+if  framework.lower() != "espidf":
+    print(" --Add menu item---")
+    curr_env = env.subst('$PIOENV')
+    curr_dir = os.path.join(env.subst('$PROJECT_LIBDEPS_DIR'), curr_env, "Robusto-PlatformIO", "scripts")
 
-if "menuconfig" not in targets.values() and framework.lower() != "espidf":
     menuconfig_cmd = "python {0} {1} ".format(
         os.path.join(curr_dir, "run_menuconfig.py"),curr_env
         )
